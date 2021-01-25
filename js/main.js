46eeeb5e8fdf747c8878df80d72c55f742c813e6
Logan Brock <36110596+lgbrock@users.noreply.github.com>
2021-01-25 16:22:33 -0600

    Update
    Code is done but can't get API to work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+
+document.querySelector('button').addEventListener('click', getPicture)
+
+function getPicture() {
+    const inputVal = document.querySelector('input').value
+    const url = 'https://api.nasa.gov/planetary/apod?api_key=iGFKXujRNK1iFRGCGsz9XudMSUGs8gBWzxQYZA0N' + inputVal
+
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            let dateEnter = data.dates[0].strDate
+            let datePicture = data.dates[0].strPicture
+            let dateDescription = data.dates[0].strDescription
+            document.querySelector('h2').innerText = dateEnter
+            document.querySelector('img').src = datePicture
+            document.querySelector('h3').innerText = dateDescription
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
+
+//enter date, get picture of the day from said date
